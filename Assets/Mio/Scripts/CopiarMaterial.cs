0e0603ca8a860946fed716b4cf6131e3e4806ee2
aleja <aleja>
2019-04-11 19:42:06 -0300

    agrego scripts

--- Assets/Mio/Scripts/CopiarMaterial.cs
+++ Assets/Mio/Scripts/CopiarMaterial.cs
@@ -15,15 +15,4 @@ public class CopiarMaterial : MonoBehaviour
         }
     }
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
---
+++ Assets/Mio/Scripts/TransformFollower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TransformFollower : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void Update()
+    {
+        Refresh();
+    }
+
+
+    public void Refresh()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+            return;
+        }
+
+		float velocidad = 1 * Time.deltaTime;
+        Vector3 offset = Vector3.Scale(target.position - transform.position, new Vector3(velocidad,velocidad,velocidad));
+		transform.position += offset;
+    }
+}
