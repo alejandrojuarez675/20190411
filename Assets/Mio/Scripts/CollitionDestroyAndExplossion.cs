52d85c5518d07a44c4a478371fd8bf35a8e6b1d0
aleja <aleja>
2019-04-11 19:59:46 -0300

    objeto que sigue y explota

---
+++ Assets/Mio/Scripts/CollitionDestroyAndExplossion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollitionDestroyAndExplossion : MonoBehaviour
+{
+
+	public GameObject explosion;
+    public string nameObjetoQueToco;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == this.nameObjetoQueToco)
+        {
+			this.gameObject.GetComponent<Rigidbody>().AddForce(0,500,0);
+			Destroy(other.gameObject);
+
+			GameObject explosion = Instantiate(this.explosion, transform.position, Quaternion.identity);
+			Destroy(explosion,1);
+        }
+    }
+
+}
